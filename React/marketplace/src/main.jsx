6d7eceedf3214fc0de3c6a9f469a73ca449e0f6f
icdfh <madiyar.jetibergen@narxoz.kz>
2026-06-23 19:03:47 +0500

    marketplace

--- React/marketplace/src/main.jsx
+++ React/marketplace/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
+import {Provider} from "@reduxjs/toolkit"
+import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
+  
   
 )
