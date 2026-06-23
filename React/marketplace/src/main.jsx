4f70f2abb19ba01af11724ec87708c982e85195d
icdfh <madiyar.jetibergen@narxoz.kz>
2026-06-23 20:16:21 +0500

    product detail

--- React/marketplace/src/main.jsx
+++ React/marketplace/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-import {Provider} from "@reduxjs/toolkit"
+import {Provider} from "react-redux"
 import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
